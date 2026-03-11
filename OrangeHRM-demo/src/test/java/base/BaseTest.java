14e471de68162446226eb6f81d9bbc6e1a7d1d00
Madhav005 <madhavmathi142k5@gmail.com>
2026-03-11 20:03:47 +0530

    First commit

---
+++ OrangeHRM-demo/src/test/java/base/BaseTest.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+    }
+}
